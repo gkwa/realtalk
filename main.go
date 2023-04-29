c64196c1283f28f742fba8ab6e074f999fe16823
Taylor Monacelli <taylormonacelli@gmail.com>
2023-04-29 08:01:38 -0700

    refactored

--- main.go
+++ main.go
@@ -8,46 +8,64 @@ import (
 )
 
 func main() {
-	// Find the current user
+	setupConfig()
 
-	// call multiple times to add many search paths
+	printConfig("ContentDir", "LayoutDir", "Taxonomies.tag")
+}
+
+func setupConfig() {
+	addConfigPaths()
+	setConfigNameAndType()
+	setDefaultValues()
+
+	if err := viper.ReadInConfig(); err != nil {
+		handleConfigReadError(err)
+	}
+}
+
+func addConfigPaths() {
 	viper.AddConfigPath("$HOME")
 	viper.AddConfigPath(".")
+}
 
+func setConfigNameAndType() {
 	viper.SetConfigName(".realtalk")
 	viper.SetConfigType("yaml")
+}
 
+func setDefaultValues() {
 	viper.SetDefault("ContentDir", "content")
 	viper.SetDefault("LayoutDir", "layouts")
 	viper.SetDefault("Taxonomies", map[string]string{"tag": "tags", "category": "categories"})
+}
 
-	// Read in the config file
-	if err := viper.ReadInConfig(); err != nil {
-		// If the config file doesn't exist, create it with default values
-		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
-			fmt.Println("Config file not found, creating it with default values...")
+func handleConfigReadError(err error) {
+	if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+		createConfigFile()
+	} else {
+		fmt.Println("Error reading config file:", err)
+		os.Exit(1)
+	}
+}
 
-			if err := viper.SafeWriteConfig(); err != nil {
-				if os.IsNotExist(err) {
-					if err := viper.WriteConfig(); err != nil {
-						fmt.Println("Error writing config file:", err)
-						os.Exit(1)
-					}
-				}
+func createConfigFile() {
+	fmt.Println("Config file not found, creating it with default values...")
+
+	if err := viper.SafeWriteConfig(); err != nil {
+		if os.IsNotExist(err) {
+			if err := viper.WriteConfig(); err != nil {
+				fmt.Println("Error writing config file:", err)
+				os.Exit(1)
 			}
-
-			fmt.Println("Config file created with default values.")
-		} else {
-			fmt.Println("Error reading config file:", err)
-			os.Exit(1)
 		}
 	}
 
-	// Get values from the config file
-	ContentDir := viper.GetString("ContentDir")
-	LayoutDir := viper.GetString("LayoutDir")
+	fmt.Println("Config file created with default values.")
+}
 
-	// Print out the values
-	fmt.Println("ContentDir:", ContentDir)
-	fmt.Println("LayoutDir:", LayoutDir)
+func printConfig(keys ...string) {
+	for _, key := range keys {
+		value := viper.Get(key)
+		fmt.Printf("%s: %v\n", key, value)
+	}
 }
